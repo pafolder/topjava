b48696569fb77b4a6d2cda581d42576cda83c067
SP <pafolder@gmail.com>
2022-10-03 18:28:02 +0500

    Filtering by recursion signature restored

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -18,14 +18,18 @@ public class UserMealsUtil {
                 new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410)
         );
         System.out.println("Filtered by recursion (Bonus):");
-        List<UserMealWithExcess> mealsFilteredByRecursion = filteredByRecursion(meals.size(), meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
+        List<UserMealWithExcess> mealsFilteredByRecursion = filteredByRecursion(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
         mealsFilteredByRecursion.forEach(System.out::println);
     }
 
     static Map<LocalDate, Integer> caloriesByDate = new HashMap<>();
     static List<UserMealWithExcess> resultList = new ArrayList<>();
 
-    static List<UserMealWithExcess> filteredByRecursion(int i, List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int dayCaloriesLimit) {
+    static List<UserMealWithExcess> filteredByRecursion(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int dayCaloriesLimit) {
+        return filteredByRecursion(meals.size(), meals, startTime, endTime, dayCaloriesLimit);
+    }
+
+        static List<UserMealWithExcess> filteredByRecursion(int i, List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int dayCaloriesLimit) {
         if (i != 0) {
             UserMeal userMeal = meals.get(--i);
             caloriesByDate.put(userMeal.getDate(), caloriesByDate.getOrDefault(userMeal.getDate(), 0) + userMeal.getCalories());
