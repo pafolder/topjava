df4926a702bd961c7eb6780c274f6bb0bcc264fc
SP <pafolder@gmail.com>
2022-10-05 17:56:32 +0500

    Complexity assessment test by JMH. -Xmx900m added

--- src/test/java/ru/javawebinar/topjava/FilterTests.java
+++ src/test/java/ru/javawebinar/topjava/FilterTests.java
@@ -2,6 +2,8 @@ package ru.javawebinar.topjava;
 
 import org.openjdk.jmh.annotations.*;
 import ru.javawebinar.topjava.model.UserMeal;
+import org.openjdk.jmh.infra.Blackhole;
+
 
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -12,13 +14,12 @@ import static ru.javawebinar.topjava.util.UserMealsUtil.*;
 
 @State(Scope.Benchmark)
 public class FilterTests {
-    @Param({"500000", "1000000", "2000000", "4000000", "8000000" })
+    @Param({"500000", "1000000", "2000000", "4000000", "8000000"})
     private int MEALS_NUMBER = 1000000;
     private List<UserMeal> meals = new ArrayList<>();
 
-   @Setup(Level.Iteration)
-   public void prepareMeals ()
-    {
+    @Setup(Level.Iteration)
+    public void prepareMeals() {
         for (int i = 0; i < MEALS_NUMBER; i++) {
             meals.add(new UserMeal(LocalDateTime.of(i % 2000,
                     1 + i % 12,
@@ -31,10 +32,10 @@ public class FilterTests {
     }
 
 
-    @Fork(value = 1, warmups = 1)
+    @Fork(value = 1, warmups = 1, jvmArgs = "-Xmx1000m")
     @Benchmark
     @BenchmarkMode(Mode.AverageTime)
-    public void cyclesPerformance() {
-        filteredByCycles(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
+    public void cyclesPerformance(Blackhole blackhole) {
+        blackhole.consume(filteredByCycles(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000));
     }
 }
